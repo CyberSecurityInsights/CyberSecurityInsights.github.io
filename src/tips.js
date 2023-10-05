d6cf82d13c696d463f10eb811281bb99f4d2cc12
slocksert <rsdlj01@gmail.com>
2023-10-05 17:46:25 -0300

    adding transition and normalizer

--- src/tips.js
+++ src/tips.js
@@ -1,121 +1,94 @@
 const quizLink = document.getElementById("quiz");
-const noticiasLink = document.getElementById("noticias")
-const tipsLink = document.getElementById("dicas")
+const noticiasLink = document.getElementById("noticias");
+const tipsLink = document.getElementById("dicas");
 const noticiasDiv = document.getElementById("newscontainer");
+const form = document.getElementById("send");
+const userInput = document.getElementById("user");
+const pwdInput = document.getElementById("pwd");
+const news = document.querySelectorAll(".news");
 
 tipsLink.addEventListener("click", function() {
-    window.location.href = "../tips.html"
-})
-
+    window.location.href = "../tips.html";
+});
 
 quizLink.addEventListener("click", function(){
     if (quizLink.textContent === "Notícias"){
-        exibirNoticias()
-    }
-    else{
-        exibirQuiz()
+        exibirNoticias();
+    } else {
+        exibirQuiz();
     }
 });
 
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
 
-function exibirQuiz(){
-    noticiasDiv.innerHTML = "";
-    quizLink.textContent = "Notícias"
+    // Check if the fields are empty
+    if (userInput.value.trim() === "" || pwdInput.value.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        // Save values in localStorage
+        localStorage.setItem("usuario", userInput.value);
+        localStorage.setItem("senha", pwdInput.value);
 
-    var formulario = document.createElement('form')
-    
-    var quizContent = document.createElement("div")
-    var title = document.createElement('h2')
-    var userInput = document.createElement('input')
-    var pwdInput = document.createElement('input')
-    var userText = document.createElement('span')
-    var pwdText = document.createElement('span')
-    var button = document.createElement('button')
-    var registerText = document.createElement('h1')
+        // Redirect to warning.html
+        window.location.href = "warning.html";
+    }
+});
 
-    userInput.className = 'user-input'
-    quizContent.className = "quiz-container"
-    title.textContent = 'Cadastre-se'
-    userText.textContent = 'Usuário'
-    pwdText.textContent = 'Senha'
-    pwdInput.className = 'user-input'
-    pwdInput.type = 'password'
-    button.className = 'quiz-button'
-    button.textContent = "Entrar"
-    registerText.textContent = "Registre-se para ter acesso ao quiz"
+function exibirQuiz() {
+    quizLink.textContent = "Notícias";
+    const h1 = document.querySelector(".quiz-title");
+    const container = document.querySelector(".quiz-container");
+    const newsElements = document.querySelectorAll(".news");
 
-    noticiasDiv.style.margin = "0"
-    noticiasDiv.style.marginBottom = "10rem"
-    noticiasDiv.appendChild(registerText)
-    noticiasDiv.appendChild(quizContent);
-    quizContent.appendChild(formulario)
-    formulario.appendChild(title)
-    formulario.appendChild(userText)
-    formulario.appendChild(userInput)
-    formulario.appendChild(pwdText)
-    formulario.appendChild(pwdInput)
-    formulario.appendChild(button)
+    // Adiciona a classe de fadeOut para os elementos de notícias
+    newsElements.forEach(element => {
+        element.classList.add("fadeOut");
+        element.classList.remove("fadeIn");
+    });
 
+    container.style.display = "block";
 
-    button.addEventListener("click", function(event) {
-        event.preventDefault(); // Impede o envio do formulário
+    setTimeout(() => {
+        newsElements.forEach(element => {
+            element.style.display = "none";
+        });
 
-        // Verifica se os campos estão vazios
-        if (userInput.value.trim() === "" || pwdInput.value.trim() === "") {
-            alert("Por favor, preencha todos os campos.");
-        } else {
-            // Salva os valores no localStorage
-            localStorage.setItem("usuario", userInput.value);
-            localStorage.setItem("senha", pwdInput.value);
+        h1.classList.remove("fadeOut");
+        h1.classList.add("fadeIn");
+        container.classList.remove("fadeOut");
+        container.classList.add("fadeIn");
+    }, 500); // Ajuste este valor para coincidir com a duração da sua animação de fadeOut
 
-            // Redireciona para quiz.html
-            window.location.href = "warning.html";
-        }
-
-    })
+    noticiasDiv.style.marginBottom = "10rem";
 }
 
-function exibirNoticias(){
-    quizLink.textContent = "Quiz"
-    noticiasDiv.innerHTML = ""
-    noticiasDiv.innerHTML = `
-    <div class="news-container" id="newscontainer">
-            <div class="news">
-                <div class="news-image">
-                    <img src="media/2023-10-01_17-17.png" alt="">
-                </div>
-                <div class="news-content">
-                    <h2>ITFORUM</h2>
-                    <h3>Phishing fez empresas brasileiras perderem dinheiro em 2022</h3>
-                    <p>E-mail continua sendo o método de ataque preferido dos criminosos, revela estudo da Proofpoint</p>
-                    <a target="_blank" href="https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/">https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/</a>
-                </div>
-            </div>
-            <div class="news">
-                <div class="news-image">
-                    <img src="https://www.hardware.com.br/wp-content/uploads/static/wp/2023/03/16/Captura-de-tela-2023-03-03-140152.png" alt="">
-                </div>
-                <div class="news-content">
-                    <h2>HARDWARE.COM.BR</h2>
-                    <h3>Brasil foi o país com o maior número de ataques de
-                        phishing por WhatsApp em 2022</h3>
-                    <p>No último ano, o Brasil foi o país mais atacado por phishing pelo WhatsApp, com mais de 76 mil
-                        tentativas de fraudes.</p>
-                    <a target="_blank" href="https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/">https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/</a>
-                </div>
-            </div>
-            <div class="news">
-                <div class="news-image">
-                    <img src="https://www.contabeis.com.br/assets/img/news/novo-golpe-cibernetico-atraves-de-phishing.webp" alt="">
-                </div>
-                <div class="news-content">
-                    <h2>CONTÁBEIS</h2>
-                    <h3>Novo golpe cibernético através de phishing</h3>
-                    <p>Neste artigo, especialista alerta novo golpe em sites de reservas de hotéis e cuidados a serem
-                        tomados.</p>
-                    <a target="_blank" href="https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/">https://itforum.com.br/noticias/phishing-empresas-brasileiras-dinheiro/</a>
-                </div>
-            </div>
-        </div>
-    `
+function exibirNoticias() {
+    quizLink.textContent = "Quiz";
+    const h1 = document.querySelector(".quiz-title");
+    const container = document.querySelector(".quiz-container");
+    const newsElements = document.querySelectorAll(".news");
+
+    h1.classList.remove("fadeIn")
+    h1.classList.add("fadeOut")
+
+    container.classList.remove("fadeIn")
+    container.classList.add("fadeOut")
+
+    h1.style.display = "none"
+    container.style.display = "none"
+
+    newsElements.forEach(element =>{
+        element.style.display = "flex"
+    })
+
+    setTimeout(() => {
+        newsElements.forEach(element=>{
+            element.classList.remove("fadeOut")
+            element.classList.add("fadeIn")
+        })
+    }, 500);
+
+    noticiasDiv.style.marginBottom = "0"
+
 }
